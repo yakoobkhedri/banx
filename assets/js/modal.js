f1560f2eb82741d82650460208b2993ddae2a066
yakoobkhedri <yakkobkhedrii@gmail.com>
2025-02-17 20:11:09 +0330

    commit message

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,8 +1,5 @@
 let closeAuthModal = document.getElementById('closeAuthModal');
 
 closeAuthModal.addEventListener('click' , function () {
-    closeAuthModal.parentElement.parentElement.parentElement.previousElementSibling.classList.remove('show');
-    closeAuthModal.parentElement.parentElement.parentElement.classList.remove('show');
-    closeAuthModal.parentElement.parentElement.parentElement.style.display = 'none';
-    closeAuthModal.parentElement.parentElement.parentElement.previousElementSibling.style.display = 'none';
+    closeAuthModal.parentElement.parentElement.classList.remove('active');
 })
