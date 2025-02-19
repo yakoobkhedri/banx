cba9d45c5c886065c6ca482637cdde95a43a57d6
yakoobkhedri <yakkobkhedrii@gmail.com>
2025-02-19 16:52:30 +0330

    commit message

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -14,8 +14,9 @@ Highcharts.stockChart('container', {
 
     plotOptions: {
         series: {
+            allowPointSelect: true,
             cumulative: true,
-            pointStart: '2023-01-01',
+            pointStart: '2025-01-01',
             pointIntervalUnit: 'day'
         }
     },
@@ -26,29 +27,44 @@ Highcharts.stockChart('container', {
 
     tooltip: {
         pointFormat: '<span style="color:{series.color}">{series.name}</span>' +
-            ': <b>{point.y}</b> (${point.cumulativeSum})<br/>',
-        changeDecimals: 2,
-        valueDecimals: 2,
-        // valuePrefix: '$',
+            ': <b>{point.y}</b>',
+        changeDecimals: 3,
+        valueDecimals: 3,
+        valuePrefix: 'تومان',
         // valueSuffix: ' USD'
     },
 
     xAxis: {
         minRange: 3 * 24 * 36e5,
-        max: '2023-01-06'
+        // max: '2023-01-06'
     },
-
-    series: [{
-        name: 'The Local Bakery',
+    yAxis: {
+        // tickInterval: 1
+    },
+    series: [
+        {
+        lineWidth: 2,
+        name: 'سود',
+        color: '#B8C14B',
         data: [
             0, 5, 10,15, 20, 5, 10, 15,
+            5, 10, 0, 5, 10,15, 20, 5, 10, 15,
+            5, 10, 0, 5, 10,15, 20, 5, 10, 15,
+            5, 10, 0, 5, 10,15, 20, 5, 10, 15,
             5, 10
         ]
-    }, {
-        name: 'The Local Fishmarket',
+    },
+     {
+        lineWidth: 2,
+        name: 'سرمایه',
+        color: '#1B76A8',
         data: [
             10, 15, 0,5, 10, 15, 20, 5,
+            15, 20, 10, 15, 0,5, 10, 15, 20, 5,
+            15, 20,10, 15, 0,5, 10, 15, 20, 5,
+            15, 20, 10, 15, 0,5, 10, 15, 20, 5,
             15, 20
         ]
-    }]
+    }
+]
 });
