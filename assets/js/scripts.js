7054eccd25c695a498a1f886a97689f9da6a3c59
yakoobkhedri <yakkobkhedrii@gmail.com>
2025-02-22 19:43:21 +0330

    commit message

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -15,10 +15,13 @@ var wellcome = new Swiper(".wellcome", {
     reachEnd: function () {
         document.querySelector('.swiper-button-prev').classList.add('hidden');
         document.querySelector('.swiper-button-next').classList.add('hidden');
+        document.querySelector('.swiper-pagination').classList.add('hidden');
+        document.querySelector('.account').classList.remove('hidden');
     },
     fromEdge: function () {
         document.querySelector('.swiper-button-prev').classList.remove('hidden');
         document.querySelector('.swiper-button-next').classList.remove('hidden');
+        document.querySelector('.swiper-pagination').classList.remove('hidden');
     }
 }
 });
